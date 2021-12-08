c4b68a81abb84ed734a51bae8d212894ef045f49
Mark Mead <50486078+markmead@users.noreply.github.com>
2021-12-08 10:33:01 +0000

    feat/analytics (#1)
    * Initial commit
    
    * Remove <Head/>

--- pages/_document.js
+++ pages/_document.js
@@ -8,26 +8,39 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
-      image:
-        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+      title: 'Shopify Developer Essex | Mark Mead',
+      description:
+        'Building ecommerce websites with Shopify, powered by Tailwind CSS, Alpine JS and Ruby on Rails.'
+      // image:
+      //   'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
     }
 
     return (
       <Html lang="en">
         <Head>
+          <script src="https://www.googletagmanager.com/gtag/js?id=G-6JVZMZE949" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-6JVZMZE949');
+            `
+            }}
+          />
           <meta name="robots" content="follow, index" />
           <meta name="description" content={meta.description} />
           <meta property="og:site_name" content={meta.title} />
           <meta property="og:description" content={meta.description} />
           <meta property="og:title" content={meta.title} />
-          <meta property="og:image" content={meta.image} />
+          {/* <meta property="og:image" content={meta.image} /> */}
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@yourname" />
           <meta name="twitter:title" content={meta.title} />
           <meta name="twitter:description" content={meta.description} />
-          <meta name="twitter:image" content={meta.image} />
+          {/* <meta name="twitter:image" content={meta.image} /> */}
         </Head>
         <body>
           <Main />
