bb18c47dc50fef5418cc78738355269a2a70b0a3
markmead <itsmarkmead@gmail.com>
2022-07-07 17:51:26 +0100

    Bump - Nextra

--- pages/_document.js
+++ pages/_document.js
@@ -32,18 +32,6 @@ class MyDocument extends Document {
             `
             }}
           />
-
-          <meta name="description" content={meta.description} />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content={meta.url} />
-          <meta property="og:title" content={meta.title} />
-          <meta property="og:description" content={meta.description} />
-          <meta property="og:image" content={`${meta.url}social.jpg`} />
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content={meta.url} />
-          <meta property="twitter:title" content={meta.title} />
-          <meta property="twitter:description" content={meta.description} />
-          <meta property="twitter:image" content={`${meta.url}social.jpg`} />
         </Head>
 
         <body>
