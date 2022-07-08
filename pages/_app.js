3501871eb7a4627612e634fdcd7610f79d7c86df
Mark Mead <50486078+markmead@users.noreply.github.com>
2022-07-08 15:11:05 +0100

    update/site (#59)
    * Bump - Nextra
    
    * Fix - Missing syntax highlighting

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
-import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
+import Script from 'next/script'
+
+import 'nextra-theme-blog/style.css'
 
 import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
+  const getLayout = Component.getLayout || ((page) => page)
+
   return (
     <>
       <Head>
@@ -13,8 +17,22 @@ export default function Nextra({ Component, pageProps }) {
           title="RSS"
           href="/feed.xml"
         />
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6JVZMZE949"
+          async
+        />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer=window.dataLayer||[]
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js',new Date)
+              gtag('config','G-6JVZMZE949')`
+          }}
+        />
       </Head>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </>
   )
 }
--- theme.config.js
+++ theme.config.js
@@ -3,9 +3,36 @@ const YEAR = new Date().getFullYear()
 export default {
   titleSuffix: ' - Mark Mead',
 
+  head: ({ meta }) => {
+    return (
+      <>
+        <meta name="author" content="Mark Mead" />
+        <link rel="canonical" href="https://www.markmead.dev" />
+        <meta name="title" content={meta.title} />
+        <meta property="description" content={meta.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:url" content="https://www.markmead.dev" />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:site" content="@shuding_" />
+        <meta property="twitter:title" content={meta.title} />
+        <meta property="twitter:description" content={meta.description} />
+        <meta property="twitter:url" content="https://www.markmead.dev" />
+        <meta
+          property="twitter:image"
+          content="https://www.markmead.dev/social.png"
+        />
+      </>
+    )
+  },
+
   footer: (
-    <small className="block mt-12">
-      <time>{YEAR}</time> © Mark Mead.
+    <small className="flex justify-between mt-12">
+      <span>
+        <time>{YEAR}</time> © Mark Mead.
+      </span>
+      <a href="/feed.xml">RSS</a>
     </small>
   )
 }
